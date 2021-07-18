a49a5e309f0fcfd6fc6b6e6d14b4d44f187eafe4
Mohammad Arif <danielharold21@gmail.com>
2021-07-18 17:17:30 +0530

    This is our third commit

--- app/src/main/java/com/example/gitpractice/MainActivity.kt
+++ app/src/main/java/com/example/gitpractice/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         println("This is contained in my second commit")
+
+        today is a good day
     }
 
 
